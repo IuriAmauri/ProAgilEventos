0ffdaf5db9a94323f30e46f4b3805dc7989e281f
Iuri Amauri Girolometo <iuri.girolometo@benner.com.br>
2019-07-19 13:20:29 -0300

    Imagens e filtro da grid

--- ProAgil-App/src/app/Eventos/Eventos.component.ts
+++ ProAgil-App/src/app/Eventos/Eventos.component.ts
@@ -2,12 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-Eventos',
+  selector: 'app-eventos',
   templateUrl: './Eventos.component.html',
   styleUrls: ['./Eventos.component.css']
 })
 export class EventosComponent implements OnInit {
-  eventos: any;
+  imagemLargura = 50;
+  imagemMargem = 2;
+  mostrarImagem = false;
+
+  eventosFiltrados: any = [];
+  eventos: any = [];
+  FiltroLista: string;
+
+  get fitroLista(): string {
+    return this.FiltroLista;
+  }
+
+  set filtroLista(filtro: string) {
+    this.FiltroLista = filtro;
+    this.eventosFiltrados = this.filtrarEventos(this.FiltroLista);
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -15,12 +30,22 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  alternarImagem() {
+    this.mostrarImagem = !this.mostrarImagem;
+  }
+
   getEventos() {
     this.http.get('http://localhost:5000/api/values').subscribe(response => {
       this.eventos = response;
+      this.eventosFiltrados = response;
       console.log(response);
     }, error => {
       console.log(error);
     });
+  }
+
+  filtrarEventos(filtrarPor: string) {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1);
   }
 }
